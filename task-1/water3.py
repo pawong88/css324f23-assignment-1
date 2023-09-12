0668268f71629f06c60248ef2c0abbbe10f103ad
Panipak Wongbubpha <6422781433@g.siit.tu.ac.th>
2023-09-12 12:46:23 +0700

    completed

--- task-1/water3.py
+++ task-1/water3.py
@@ -1,9 +1,31 @@
 def initial_state():
-    return (0, 0, 0)
+    return (8, 0, 0)
 
 def is_goal(s):
-    return True
+    return s[0] == 4 and s[1] == 4
 
 def successors(s):
     x, y, z = s
-    return []
+    list = []
+    if x > 0:
+        if y < 5:
+            cost = min(5-y, x)
+            list.append([(x-cost, y+cost, z),cost])
+        if z < 3:
+            cost = min(3-z, x)
+            list.append([(x-cost, y, z+cost),cost])
+    if y > 0:
+        if x < 8:
+            cost = min(8-x, y)
+            list.append([(x+cost, y-cost, z),cost])
+        if z < 3:
+            cost = min(3-z, y)
+            list.append([(x, y-cost, z+cost),cost])
+    if z > 0:
+        if x < 8:
+            cost = min(8-x, z)
+            list.append([(x+cost, y, z-cost),cost])
+        if y < 5:
+            cost = min(5-y, z)
+            list.append([(x, y+cost, z-cost),cost])
+    return list
--- task-2/eight_puzzle.py
+++ task-2/eight_puzzle.py
@@ -51,4 +51,12 @@ def h1(s):
 def h3(s):
     # implement this function
     board, _, _ = s
-    return 0
+    result = 0
+    for idx in range(0, 9):
+        if (board[idx] != 0):
+            if ((board[idx]-1)//3 != idx//3):
+                result += 1
+            if ((board[idx]-1)%3 != idx%3):
+                result += 1
+    return result
+
